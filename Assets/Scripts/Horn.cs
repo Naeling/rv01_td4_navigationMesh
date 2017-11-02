35e1d06ca15b9e2a57e74b41adc4eac62cd2a51a
Louis <louis.groisne@gmail.com>
2017-11-02 14:19:39 +0100

    Horn sound when the car is near a player Tag

---
+++ Assets/Scripts/Horn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Horn : MonoBehaviour {
+
+    private AudioSource audioSource;
+    public AudioClip hornClip;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Collision with a player");
+            audioSource.PlayOneShot(hornClip);
+        }
+    }
+}
